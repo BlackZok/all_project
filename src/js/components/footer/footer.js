e12321239501dd3ff4779fe4a2edd6d9c3b080e7
Evgeniy Kozlov <akinfeev777.97@mail.ru>
2024-03-09 12:00:18 +0300

    first

--- src/js/components/footer/footer.js
+++ src/js/components/footer/footer.js
@@ -1,6 +1,8 @@
-import {generalFooter} from "./footerModel";
+import {generalFooter, generalFooter_2} from "./footerModel";
 
 const footer = document.querySelector('#footer');
+
+const footer_2 = document.querySelector('#footer_2');
 
 let footerHtml = generalFooter.map((a) => {
     return `
@@ -8,9 +10,25 @@ let footerHtml = generalFooter.map((a) => {
     <div class="col-md-4">${a.text_1}</div>
     <div class="col-md-4">${a.text_2}</div>
     <div class="col-md-4">${a.text_3}</div>
+
+    <div class="col-md-4">${a.p_1}</div>
+    <div class="col-md-4">${a.p_2}</div>
+    <div class="col-md-4">${a.p_3}</div>
+   
     
     `
 }).join(" ");
 
-footer.insertAdjacentHTML('beforeend', footerHtml);
 
+
+let footerHtml_2 = generalFooter_2.map((a) => {
+    return `
+        <div class="col-md-4">${a.a_1}</div>
+        <div class="col-md-4">${a.a_2}</div>
+        <div class="col-md-4">${a.a_3}</div>
+    `
+}).join(" ");
+
+
+footer.insertAdjacentHTML('beforeend', footerHtml);
+footer_2.insertAdjacentHTML('beforeend', footerHtml_2);
--- src/js/components/footer/footerModel.js
+++ src/js/components/footer/footerModel.js
@@ -1,9 +1,23 @@
 export const generalFooter = [
+
     {
         text_1: 123,
         text_2: 456,
-        text_3: 789
+        text_3: 789,
+        
+        p_1: 3333,
+        p_2: 4444,
+        p_3: 5555
+    },
+
+]
+
+export const generalFooter_2 = [
+
+    {
+        a_1: 111,
+        a_2: 222,
+        a_3: 333
     },
     
-
 ]
